033da296fbc3ba3dbdf3e1c48143d77d97f54f96
Ayush <a>
2021-07-04 23:45:14 +0530

    Add Viewers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.css';
 import ImageSlider from './ImageSlider';
+import Viewers from './Viewers';
 
 function Home() {
     return (
         <main className='homeContainer'>
             <ImageSlider />
+            <Viewers />
         </main>
     );
 }
