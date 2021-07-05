feee53362e47ff150412eb20469c080da224dc18
Ayush <a>
2021-07-05 12:21:49 +0530

    add details page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { Switch, BrowserRouter, Link, Route } from 'react-router-dom';
+import Details from './components/Details';
 
 function App() {
     return (
         <div>
             <Header />
-            <Home />
+            <Switch>
+                <Route path='/details'>
+                    <Details />
+                </Route>
+                <Route path='/home'>
+                    <Home />
+                </Route>
+            </Switch>
         </div>
     );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Home.css';
 import ImageSlider from './ImageSlider';
 import Viewers from './Viewers';
+import Movies from './Movies';
 
 function Home() {
     return (
         <main className='homeContainer'>
             <ImageSlider />
             <Viewers />
+            <Movies />
         </main>
     );
 }
